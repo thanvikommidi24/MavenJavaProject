c62df7b8b69c0970721fcc468cb6d515db6130fd
thanvi <thanvikommidi24@gmail.com>
2024-11-14 21:19:32 +0530

    First commit to maven java project

---
+++ src/main/java/SEGroup/MvenJavaProject/App.java
@@ -0,0 +1,15 @@
+package SEGroup.MvenJavaProject;
+
+/**
+ * hello world!
+ * 
+ * 
+ */
+
+public class App {
+	public static void main(String[] args )
+	{
+		System.out.println("This is the first Maven Java Project in Eclipse ");
+	}
+
+}
